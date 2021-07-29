8012287be837efe2a41b496633d47c3c4c9a2822
cora <coradelamouche@gmx.ch>
2021-07-29 05:14:36 +0200

    revert last commit - nothing gets ever sent
    This reverts commit 9a9900e0010df7ab7dcbba9980344915f4ef2d4c.

--- init.lua
+++ init.lua
@@ -117,10 +117,9 @@ minetest.chat_send_all = function(message)
     discord.send(message)
 end
 
---this is handled by the anti spam plugin now
---minetest.register_on_chat_message(function(name, message)
---    discord.send(('<%s>: %s'):format(name, message))
---end)
+minetest.register_on_chat_message(function(name, message)
+    discord.send(('<%s>: %s'):format(name, message))
+end)
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
